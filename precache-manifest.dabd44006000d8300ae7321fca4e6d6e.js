498c78a0fae2e5d6b8ad1ca91af04ac9c2567272
Basir <basir.jafarzadeh@gmail.com>
2019-06-21 17:57:15 +0430

    Updates

---
+++ precache-manifest.dabd44006000d8300ae7321fca4e6d6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10b5f19b58c09655b5eb2198f5c54a3",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "87ddcde5dc6d6d2122c2",
+    "url": "/tic-tac-toe/static/css/main.60b8e82c.chunk.css"
+  },
+  {
+    "revision": "f2bf82953ccd6cf2478e",
+    "url": "/tic-tac-toe/static/js/2.087e00c1.chunk.js"
+  },
+  {
+    "revision": "87ddcde5dc6d6d2122c2",
+    "url": "/tic-tac-toe/static/js/main.54064c94.chunk.js"
+  },
+  {
+    "revision": "72d2a57eca9de8d97851",
+    "url": "/tic-tac-toe/static/js/runtime~main.1fdedca4.js"
+  }
+]);
